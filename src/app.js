c1eb8640409665d0639466c10f00b7a3a62cacfc
Vikas <ordersmego@gmail.com>
2026-02-19 17:02:13 +0530

    tinder_yuv || Read data from the URL

--- src/app.js
+++ src/app.js
@@ -2,9 +2,16 @@ const express = require('express')
 
 const app = express();
 
-app.get("/user/:age/:name/:department",(req,res)=>{
-    console.log(req.params);
-    console.log(req.query);
+// app.get("/user",(req,res)=>{
+//     console.log(req.query);  // used to read valuse from the URL in form of : 
+//     res.send({
+//         userName : "Yuvraj Singh",
+//         department : "Dev Team"
+//     })
+// });
+
+app.get("/user:age/:name/:department",(req,res)=>{
+    console.log(req.params); //used to read values from the URL in form of : http://localhost:3001/user/2112/yuvraj/developer
     res.send({
         userName : "Yuvraj Singh",
         department : "Dev Team"
